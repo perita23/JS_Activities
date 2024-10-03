bd05dfaf83fbad2a7eb71d744cf580fc993a7e5b
perita23 <hugobedmar10@gmail.com>
2024-10-03 15:27:58 +0200

    CHANGES

--- act2.js
+++ act2.js
@@ -1,1 +1,5 @@
-document.getElementById("maxIntVal").setHTMLUnsafe("")
+document.getElementById("maxIntVal").setHTMLUnsafe(Number.MAX_VALUE)
+document.getElementById("maxSHeight").setHTMLUnsafe(screen.height)
+document.getElementById("maxWHeight").setHTMLUnsafe(window.innerHeight)
+document.getElementById("getUrl").setHTMLUnsafe(document.URL)
+
