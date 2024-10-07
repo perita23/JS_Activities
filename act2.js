5ce281b0f99227f3f0bdb34d536983ba1cf3f31d
perita23 <hugobedmar10@gmail.com>
2024-10-07 09:03:59 +0200

    cron

--- act2.js
+++ act2.js
@@ -3,3 +3,24 @@ document.getElementById("maxSHeight").setHTMLUnsafe(screen.height)
 document.getElementById("maxWHeight").setHTMLUnsafe(window.innerHeight)
 document.getElementById("getUrl").setHTMLUnsafe(document.URL)
 
+/*----------------Segunda Parte-----------------*/
+
+const minuts = document.getElementById("minuts")
+const seconds = document.getElementById("seconds")
+
+
+function startCron() {
+    if (seconds.value <= 59) {
+        if (seconds.value == 0) {
+            if (minuts.value == 0 && seconds.value == 0) {
+                return null
+            }
+            minuts.value = minuts.value-1;
+            seconds.value = 59;
+        }
+        seconds.value = seconds.value-1;
+    } else {
+        alert("No puedes mas de 59 segundos ")
+    }
+    
+}
