4f9d78d7fe5f5514d66677507212697773909f8c
perita23 <hugobedmar10@gmail.com>
2024-10-07 09:50:49 +0200

    cron finished

--- act2.js
+++ act2.js
@@ -5,22 +5,79 @@ document.getElementById("getUrl").setHTMLUnsafe(document.URL)
 
 /*----------------Segunda Parte-----------------*/
 
-const minuts = document.getElementById("minuts")
-const seconds = document.getElementById("seconds")
+const minutsInput = document.getElementById("minuts");
+const secondsInput = document.getElementById("seconds");
 
+let interval;
+let isPaused = false;
+let remainingMinuts;
+let remainingSeconds;
+let alarma = new Audio("/DRUMC0.WAV");
+alarma.loop = true;
+
+function playAlarm() {
+    alarma.play();
+}
+
+function stopAlarm() {
+    alarma.pause();
+    alarma.currentTime = 0;
+}
 
 function startCron() {
-    if (seconds.value <= 59) {
-        if (seconds.value == 0) {
-            if (minuts.value == 0 && seconds.value == 0) {
-                return null
-            }
-            minuts.value = minuts.value-1;
-            seconds.value = 59;
-        }
-        seconds.value = seconds.value-1;
-    } else {
-        alert("No puedes mas de 59 segundos ")
+    if (interval) {
+        clearInterval(interval);
     }
-    
+
+    let minuts = parseInt(minutsInput.value);
+    let seconds = parseInt(secondsInput.value);
+
+    if (isNaN(minuts) || isNaN(seconds) || minuts < 0 || seconds < 0 || seconds > 59) {
+        alert("Por favor, introduce un tiempo válido.");
+        return;
+    }
+
+    if (isPaused && remainingSeconds != 0 && remainingMinuts != 0) {
+        minuts = remainingMinuts;
+        seconds = remainingSeconds;
+    }
+
+    interval = setInterval(function () {
+        if (seconds == 0) {
+            if (minuts == 0) {
+                clearInterval(interval);
+                playAlarm();
+                return;
+            } else {
+                minuts--;
+                seconds = 59;
+            }
+        } else {
+            seconds--;
+        }
+
+        minutsInput.value = minuts;
+        secondsInput.value = seconds;
+    }, 1000);
+
+    isPaused = false;
+}
+
+function pauseCron() {
+    if (interval) {
+        clearInterval(interval);
+        isPaused = true;
+        remainingMinuts = minutsInput.value
+        remainingSeconds = secondsInput.value
+    }
+}
+
+function resetCron() {
+    clearInterval(interval);
+    minutsInput.value = 0;
+    secondsInput.value = 0;
+    isPaused = false;
+    remainingMinuts = null
+    remainingSeconds = null
+    stopAlarm();
 }
